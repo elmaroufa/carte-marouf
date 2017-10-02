3773f7a393e83eeed95b512880673f5c3a3c6374
Saly Abbo <elmaroufa@gmail.com>
2017-10-02 12:35:22 +0100

    Modification fichier app.php pour le nom de l'application

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
  <div class="container">
- <h1>What is a laracarte</h1>
- <p>Laracarte is a clone App a <a href="https://Laramap.com">Laramap</a></p>
+ <h1>What is a {{config('app.name')}}</h1>
+ <p>{{config('app.name')}} is a clone App a <a href="https://Laramap.com">Laramap</a></p>
    <div class="row">
    <div class="col-md-6">
    <p class="alert alert-warning">
@@ -14,7 +14,7 @@ This app has been build by elmazaar</strong>
    </div>
    <p>Feel free to help improve code source in <a href="https://github.com/elmaroufa/carte-marouf">source</a></p><hr>
    <h2>Whath is a Laramap</h2>
-   <p>Laramap in the website whhich laracart about_patch</p>
+   <p>Laramap in the website whhich {{config('app.name')}} about_patch</p>
    <p>more info <a href="#">here</a></p>
  </div>
 @stop
